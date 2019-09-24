6d92229101911ebc73448c5566c52f23a7702015
Savannah C <sgcorrell@gmail.com>
2019-09-24 15:41:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function writeCards(array, occasion) {
   for (let i = 0; i < array.length; i++) {
     messages.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`)
   }
-  
+  return messages
 }
