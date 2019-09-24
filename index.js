0f27496b230b98905320223993d2b061635d2b61
Savannah C <sgcorrell@gmail.com>
2019-09-24 15:47:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function writeCards(array, occasion) {
-  let messages = []
+  let messages = [];
   for (let i = 0; i < array.length; i++) {
-    messages.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`)
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`);
   }
-  return messages
+  return messages;
 }
 
 function countdown(number) {
-  let n = number
+  let n = number;
   while (n >= 0) {
-    console.log(n--)
+    console.log(n--);
   }
 }
