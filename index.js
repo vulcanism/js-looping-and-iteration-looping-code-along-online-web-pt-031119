d8b6f501713b51bd87543aafb21eda1a9c8371f7
Savannah C <sgcorrell@gmail.com>
2019-09-24 15:47:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, occasion) {
+  let messages = [];
+  for (let i = 0; i < array.length; i++) {
+    messages.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`);
+  }
+  return messages;
+}
+
+function countdown(number) {
+  let n = number;
+  while (n >= 0) {
+    console.log(n--);
+  }
+}
